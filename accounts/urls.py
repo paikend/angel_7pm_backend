88315ba86f6fb0f1755ba259e002a97c08af2ecf
paikend <paikend@gmail.com>
2020-07-14 19:15:33 +0900

    Add sign-in

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+  path('sign-in/', views.sign_in),
+  # path('sign-up/', views.sign_up),
+  # path('user-profile', views.user_profile),
 ]
